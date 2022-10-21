dbecfd52a0f21a0d6b7433dd198780e905506796
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-21 02:13:20 +0000

    Update __init__.py

--- Pandas_Market_Predictor/__init__.py
+++ Pandas_Market_Predictor/__init__.py
@@ -5,4 +5,8 @@ import numpy as np
 if __name__ == "__main__" :
   
   dataset = pd.read_csv('dataset.csv')
+  MyMarketPredictor = Pandas_Market_Predictor(dataset)
+  TREND = MyMarketPredictor.Trend_Detection(["INDICATOR1","INDICATOR2"],10)
+  print("Buy Trend :",TREND['BUY'])
+  print("Sell Trend :",TREND['SELL'])
   
