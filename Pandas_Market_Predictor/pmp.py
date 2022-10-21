bf3e0f7f65ce5b3b77fe8d2b37541e479d0c1dee
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-21 05:18:41 +0000

    Update pmp.py

--- Pandas_Market_Predictor/pmp.py
+++ Pandas_Market_Predictor/pmp.py
@@ -41,6 +41,6 @@ if __name__ == "__main__" :
   df = pd.read_csv('EURJPY.csv')
   df = df.dropna(axis=0)
   MyMarketPredictor = Pandas_Market_Predictor(df)
-  TREND = MyMarketPredictor.Trend_Detection(["Indicator1","Indicator2","Indicator3"],10)
+  TREND = MyMarketPredictor.Trend_Detection(["Indicator1","Indicator2"],10)
   print("Buy Trend :",TREND['BUY'])
   print("Sell Trend :",TREND['SELL'])
