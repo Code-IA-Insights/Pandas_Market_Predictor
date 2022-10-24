ed94907a6b80b930e8943c7d1c5ba196cf723b87
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-24 18:24:32 +0000

    Update pmp.py

--- Pandas_Market_Predictor/pmp.py
+++ Pandas_Market_Predictor/pmp.py
@@ -1,5 +1,5 @@
 from Artificial_Neural_Network_Classifier import artificialneuralnetwork_classifier as ANNC
-from Awesome_Linear_Regression import linearregression as LR
+from Auto_Gradient_Boosting import AGB
 import pandas as pd
 import numpy as np
 
@@ -45,15 +45,13 @@ class Pandas_Market_Predictor :
     y1 = np.matrix(self.dataset.iloc[1:-1 , :][['support_distance']].to_numpy())
     y2 = np.matrix(self.dataset.iloc[1:-1 , :][['resistance_distance']].to_numpy()) 
     
-    Lr_support = LR(x,y1)
-    BetaS, rssS = Lr_support.leastsquare()
-    Lr_resistance = LR(x,y2)
-    BetaR, rssR = Lr_resistance.leastsquare()
+    agb_support = AGB(x,y1,0.001)
+    agb_resistance = AGB(x,y2,0.001)
     
     SIGNAL = np.matrix( self.dataset.tail(1)[indicator_list].to_numpy() )
     
-    S = self.dataset.tail(1)['Close'].values[0] - Lr_support.predict(SIGNAL)
-    R = self.dataset.tail(1)['Close'].values[0] + Lr_resistance.predict(SIGNAL)
+    S = self.dataset.tail(1)['Close'].values[0] - agb_support.predict(SIGNAL)
+    R = self.dataset.tail(1)['Close'].values[0] + agb_resistance.predict(SIGNAL)
     
     return {
       "Support" :  S,
